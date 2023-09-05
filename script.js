24af2aa96a87a22b5efaf9bb03000126a5105a72
clean <55951183+clean203@users.noreply.github.com>
2023-09-05 21:52:01 +0530

    add buttons and logic to play a round of rpc

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const prompt = require("prompt-sync")({ sigint: true });
+// const prompt = require("prompt-sync")({ sigint: true });
 
 let playerScore = 0;
 let computerScore = 0;
@@ -31,22 +31,38 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Choose your hand ");
-    let computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
-  }
-  if (playerScore > computerScore) {
-    console.log("You win this round :)");
-  } else if (computerScore > playerScore) {
-    console.log("You lose this round :(");
-  } else {
-    console.log("This round is a draw :|");
-  }
-  console.log("Final scores:");
-  console.log(`Player score: ${playerScore}`);
-  console.log(`Computer score: ${computerScore}`);
-}
+const btnRock = document.querySelector("#rock");
+btnRock.addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound("rock", computerSelection);
+});
 
-game();
+const btnPaper = document.querySelector("#paper");
+btnPaper.addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound("paper", computerSelection);
+});
+
+const btnScissors = document.querySelector("#scissors");
+btnScissors.addEventListener("click", () => {
+  let computerSelection = getComputerChoice();
+  playRound("scissors", computerSelection);
+});
+
+// function game() {
+//   let playerSelection = prompt("Choose your hand ");
+//   let computerSelection = getComputerChoice();
+//   playRound(playerSelection, computerSelection);
+//   if (playerScore > computerScore) {
+//     console.log("You win this round :)");
+//   } else if (computerScore > playerScore) {
+//     console.log("You lose this round :(");
+//   } else {
+//     console.log("This round is a draw :|");
+//   }
+//   console.log("Final scores:");
+//   console.log(`Player score: ${playerScore}`);
+//   console.log(`Computer score: ${computerScore}`);
+// }
+
+// game();
