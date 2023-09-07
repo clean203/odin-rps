294283251905818094ad6e3a65b7a1b949c917a8
clean <55951183+clean203@users.noreply.github.com>
2023-09-07 23:23:23 +0530

    added logic to check scores and declare winner

--- script.js
+++ script.js
@@ -17,72 +17,90 @@ function playRound(playerSelection, computerSelection) {
     (playerLowerCase == "scissors" && computerLowerCase == "rock") ||
     (playerLowerCase == "paper" && computerLowerCase == "scissors")
   ) {
-    console.log("You lose!");
     computerScore++;
-    // console.log(computerScore);
+    return "You lose this battle";
   } else if (
     (playerLowerCase == "paper" && computerLowerCase == "rock") ||
     (playerLowerCase == "rock" && computerLowerCase == "scissors") ||
     (playerLowerCase == "scissors" && computerLowerCase == "paper")
   ) {
-    console.log("You win!");
     playerScore++;
-    // console.log(playerScore);
+    return "You win this battle!";
   } else {
-    console.log("Draw!");
+    return "'Tis a Draw!";
   }
 }
-
-const btnRock = document.querySelector("#rock");
-btnRock.addEventListener("click", () => {
-  let computerSelection = getComputerChoice();
-  playRound("rock", computerSelection);
-  resultUpdate();
-});
-
-const btnPaper = document.querySelector("#paper");
-btnPaper.addEventListener("click", () => {
-  let computerSelection = getComputerChoice();
-  playRound("paper", computerSelection);
-  resultUpdate();
-});
-
-const btnScissors = document.querySelector("#scissors");
-btnScissors.addEventListener("click", () => {
-  let computerSelection = getComputerChoice();
-  playRound("scissors", computerSelection);
-  resultUpdate();
-});
-
 let result = document.createElement("div");
 document.body.appendChild(result);
+
 let p1 = document.createElement("p");
 p1.classList.add("line1");
 result.appendChild(p1);
+
 let p2 = document.createElement("p");
 p2.classList.add("line2");
 result.appendChild(p2);
+
+let p3 = document.createElement("p");
+p3.classList.add("line3");
+result.appendChild(p3);
+
+let p4 = document.createElement("p");
+p4.classList.add("line4");
+result.appendChild(p4);
+
 p1.textContent = `Player score - Computer score `;
 p2.textContent = `${playerScore} - ${computerScore}`;
 
-function resultUpdate() {
+let rel = () => {
+  let computerSelection = getComputerChoice();
+  let textResult = playRound("rock", computerSelection);
+  resultUpdate(textResult);
+  checkScore(playerScore, computerScore);
+};
+
+let pel = () => {
+  let computerSelection = getComputerChoice();
+  let textResult = playRound("paper", computerSelection);
+  resultUpdate(textResult);
+  checkScore(playerScore, computerScore);
+};
+
+let sel = () => {
+  let computerSelection = getComputerChoice();
+  let textResult = playRound("scissors", computerSelection);
+  resultUpdate(textResult);
+  checkScore(playerScore, computerScore);
+};
+
+const btnRock = document.querySelector("#rock");
+btnRock.addEventListener("click", rel);
+
+const btnPaper = document.querySelector("#paper");
+btnPaper.addEventListener("click", pel);
+
+const btnScissors = document.querySelector("#scissors");
+btnScissors.addEventListener("click", sel);
+
+function resultUpdate(textResult) {
   p2.textContent = `${playerScore} - ${computerScore}`;
+  p3.textContent = `${textResult}`;
 }
 
-// function game() {
-//   let playerSelection = prompt("Choose your hand ");
-//   let computerSelection = getComputerChoice();
-//   playRound(playerSelection, computerSelection);
-//   if (playerScore > computerScore) {
-//     console.log("You win this round :)");
-//   } else if (computerScore > playerScore) {
-//     console.log("You lose this round :(");
-//   } else {
-//     console.log("This round is a draw :|");
-//   }
-//   console.log("Final scores:");
-//   console.log(`Player score: ${playerScore}`);
-//   console.log(`Computer score: ${computerScore}`);
-// }
-
-// game();
+function checkScore(playerScore, computerScore) {
+  if (playerScore == 5) {
+    p3.remove();
+    p4.textContent = "You win this war!!";
+    btnRock.removeEventListener("click", rel);
+    btnPaper.removeEventListener("click", pel);
+    btnScissors.removeEventListener("click", sel);
+  } else if (computerScore == 5) {
+    p3.remove();
+    p4.textContent = "You lose this war";
+    btnRock.removeEventListener("click", rel);
+    btnPaper.removeEventListener("click", pel);
+    btnScissors.removeEventListener("click", sel);
+  } else {
+    //do nothing
+  }
+}
